7fd5e388ed3793546ee77c0bba86ccba100ab1e3
Rauno Lõhmus <rauno.lohmus@gmail.com>
2025-08-05 18:08:40 +0200

    remove duplicated interface

--- internal/repository/checklist_items_repository.go
+++ internal/repository/checklist_items_repository.go
@@ -14,16 +14,6 @@ import (
 	"github.com/raunlo/pgx-with-automapper/pool"
 )
 
-type IChecklistItemsRepository interface {
-	UpdateChecklistItem(checklistId uint, checklistItem domain.ChecklistItem) (domain.ChecklistItem, domain.Error)
-	SaveChecklistItem(checklistId uint, checklistItem domain.ChecklistItem) (domain.ChecklistItem, domain.Error)
-	SaveChecklistItemRow(checklistId uint, checklistItemId uint, row domain.ChecklistItemRow) (domain.ChecklistItemRow, domain.Error)
-	FindChecklistItemById(checklistId uint, id uint) (*domain.ChecklistItem, domain.Error)
-	DeleteChecklistItemById(checklistId uint, id uint) domain.Error
-	FindAllChecklistItems(checklistId uint, completed bool, sortOrder domain.SortOrder) ([]domain.ChecklistItem, domain.Error)
-	ChangeChecklistItemOrder(request domain.ChangeOrderRequest) (domain.ChangeOrderResponse, domain.Error)
-}
-
 type checklistItemRepository struct {
 	conn pool.Conn
 }
