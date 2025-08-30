ac747c7e1ba565fabb8d65a813b423424d7e7eb2
Rauno Lõhmus <rauno.lohmus@gmail.com>
2025-08-30 15:05:49 +0200

    update gin configuration

--- internal/deployment/gin_configuration.go
+++ internal/deployment/gin_configuration.go
@@ -16,20 +16,29 @@ func GetGinRouter(corsConfiguration CorsConfiguration) *gin.Engine {
 		panic(err)
 	}
 	router := gin.New()
-	router.Use(validator)
 	router.Use(gin.Logger())
 	router.Use(cors.New(
 		cors.Config{
-			AllowOrigins:  []string{corsConfiguration.Hostname},
-			AllowMethods:  []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-			AllowHeaders:  []string{"Origin", "Content-Type", "Authorization", "Accept"},
-			ExposeHeaders: []string{"Content-Length"},
-			//AllowCredentials: true,
-			MaxAge: 12 * time.Hour,
+			AllowOrigins:     []string{corsConfiguration.Hostname},
+			AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+			AllowHeaders:     []string{"*"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
 		}))
+
+	router.Use(func(c *gin.Context) {
+		if c.Request.Method == http.MethodOptions {
+			c.Status(http.StatusNoContent) // 204
+			c.Abort()
+			return
+		}
+		c.Next()
+	})
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
 	})
+
+	router.Use(validator)
 	return router
 }
