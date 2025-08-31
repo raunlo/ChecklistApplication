3ee89b5a6fb9d534b8cd5a296006ff1445e20390
Rauno Lõhmus <32277590+raunlo@users.noreply.github.com>
2025-08-31 03:48:54 +0200

    remove init_stub (#30)

--- internal/deployment/init_stub.go
+++
@@ -1,8 +0,0 @@
-//go:build test
-
-package deployment
-
-// Init provides a no-op implementation for tests.
-func Init(configuration ApplicationConfiguration) Application {
-	return Application{}
-}
